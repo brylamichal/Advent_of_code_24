e0baba14c354db44c88e4b75851f99641cf487c5
brylamichal_personal <brylamichal97@gmail.com>
2024-12-09 22:47:32 +0100

    Update file

--- 05_day_code.py
+++ 05_day_code.py
@@ -1,4 +1,5 @@
 import os
+
 dir_name = os.path.dirname(__file__)
 dir_file = dir_name + '\\input\\05_day_input.txt'
 
@@ -28,5 +29,70 @@ for x in temp_order_print:
     temp_lst = x.split(",")
     order_print.append([int(i) for i in temp_lst])
 
-print(order_print)
+print(order_print)  # list from first task
 print(order_rule)
+
+d = {}
+for el in order_rule:
+    if str(el[0]) in d:
+        d[str(el[0])].append(el[1])
+    else:
+        d[str(el[0])] = [el[1]]
+
+print(d)
+
+
+def check_rule(print_lst):
+    add = False
+    length = len(print_lst)
+    for i, page in enumerate(print_lst):
+        if i == (length - 1):
+            continue
+        elif not str(page) in d:
+            return [False, i]
+        else:
+            temp_lst = []
+            for j in range(i + 1, length):
+                temp_lst.append(print_lst[j] in d[str(page)])
+            if all(temp_lst):
+                add = True
+            else:
+                return [False, i]
+    return [add, None]
+
+
+final_lst = []
+second_part_lst = []
+for print_lst in order_print:
+    if check_rule(print_lst)[0]:
+        final_lst.append(print_lst)
+    else:
+        second_part_lst.append(print_lst)
+
+print(final_lst)
+
+result = 0
+for l in final_lst:
+    result += l[int((len(l)-1)/2)]
+
+print(result)  # answer for first part
+
+print(second_part_lst)
+
+correct_lst = []
+for l in second_part_lst:
+    while not check_rule(l)[0]:
+        new_lst = l.copy()
+        idx = check_rule(l)[1]
+        new_lst.append(l[idx])
+        new_lst.pop(idx)
+        l = new_lst
+    correct_lst.append(l)
+
+print(correct_lst)
+
+result_sorted = 0
+for l in correct_lst:
+    result_sorted += l[int((len(l)-1)/2)]
+
+print(result_sorted)  # answer for the second part

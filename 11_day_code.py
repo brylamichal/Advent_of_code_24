9378bd301609382c639b7b0a480f23f28c4a5b76
brylamichal_business <brylamichal97@gmail.com>
2024-12-18 12:05:41 +0100

    Upload finished day 11 part 02

--- 11_day_code.py
+++ 11_day_code.py
@@ -24,21 +24,18 @@ class Stone:
         self.recurrence_blinking(n=self.blinks)
         self.count_stones = len(self.changed_stones)
 
-    @staticmethod
-    def rule_first():
+    def rule_first(self):
         return "1"
 
-    @staticmethod
-    def rule_second(d):
+    def rule_second(self, d):
         return [d[:(int(len(d)/2))], str(int(d[int((len(d)/2)):]))]
 
-    @staticmethod
-    def rule_third(d):
+    def rule_third(self, d):
         return str(int(d) * 2024)
 
     def recurrence_blinking(cls, n):
         if n > 0:
-            print(f"{cls.number}:{n}")
+            # print(f"{cls.number}:{n}")
             temp_lst = []
             for el in cls.changed_stones:
                 if el == "0":
@@ -64,26 +61,53 @@ class Stone:
             n -= 1
 
 
-# NUM_BLINKS = 25
-# lst_stones = []
-# for stone in input_lst:
-#     lst_stones.append(Stone(number=stone, blinks=NUM_BLINKS))
-#
-# answer = 0
-# for stone in lst_stones:
-#     answer += stone.count_stones
-#
-# print(answer)  # answer_part01 = 216042
+NUM_BLINKS = 4
+lst_stones = []
+for stone in input_lst:
+    lst_stones.append(Stone(number=stone, blinks=NUM_BLINKS))
 
-from collections import defaultdict
+answer = 0
+for stone in lst_stones:
+    answer += stone.count_stones
 
-stones = defaultdict(int)
+print(answer)  # answer_part01 = 216042
 
-print(stones)
+stones = dict()
 
 for stone in input_lst:
-    stones[int(stone)] += 1
+    stones[int(stone)] = 1
 
+temp_stones = stones.copy()
+blinks = 75
+for i in range(blinks):
+    for stone in stones:
+        if stones[stone] == 0: continue
+        else:
+            if stone == 0:
+                temp_stones[0] -= stones[stone]
+                if 1 in temp_stones: temp_stones[1] += stones[stone]
+                else: temp_stones[1] = stones[stone]
+            elif len(str(stone)) % 2 == 0:
+                mid = int(len(str(stone))/2)
+                v1 = int(str(stone)[:mid])
+                v2 = int(str(stone)[mid:])
+                temp_stones[stone] -= stones[stone]
+                if v1 in temp_stones: temp_stones[v1] += stones[stone]
+                else: temp_stones[v1] = stones[stone]
+                if v2 in temp_stones: temp_stones[v2] += stones[stone]
+                else: temp_stones[v2] = stones[stone]
+            else:
+                v = stone * 2024
+                temp_stones[stone] -= stones[stone]
+                if v in temp_stones: temp_stones[v] += stones[stone]
+                else: temp_stones[v] = stones[stone]
 
+    stones = temp_stones.copy()
 
 print(stones)
+
+answer = 0
+for count in stones.values():
+    answer += count
+
+print(answer)
